de810543b2232d480df9cea208eff2ab38352212
ericborn <eric.born85@gmail.com>
2019-03-20 21:47:00 -0700

    Add files via upload

---
+++ imperial.sql
@@ -0,0 +1,31 @@
+--============================================= 
+-- Author:    Eric Born 
+-- Create date: 18 March 2019
+-- Last Modified:
+-- Description: Used to insert all of the zip codes and regions for Imperial County
+-- =============================================
+
+INSERT INTO region
+(
+ZIP, Region, area, county
+)
+VALUES
+ (92222,	'Bard', NULL,	'Imperial')
+,(92227,	'Brawley	',NULL, 'Imperial')
+,(92231,	'Calexico',NULL,	'Imperial')
+,(92232,	'Calexico',NULL,	'Imperial')
+,(92233,	'Calipatria',NULL,	'Imperial')
+,(92243,	'El Centro',NULL,	'Imperial')
+,(92244,	'El Centro',NULL,	'Imperial')
+,(92249,	'Heber',NULL,	'Imperial')
+,(92250,	'Holtville',NULL,	'Imperial')
+,(92251,	'Imperial',NULL,	'Imperial')
+,(92257,	'Niland',NULL,	'Imperial')
+,(92259,	'Ocotillo',NULL,	'Imperial')
+,(92266,	'Palo Verde',NULL,	'Imperial')
+,(92273,	'Seeley',NULL,	'Imperial')
+,(92275,	'Thermal',NULL,	'Imperial')
+,(92275,	'Salton City',NULL,	'Imperial')
+,(92281,	'Westmorland',NULL,	'Imperial')
+,(92283,	'Felicity',NULL,	'Imperial')
+,(92283,	'Winterhaven',NULL,	'Imperial')
---
+++ region.sql
@@ -0,0 +1,30 @@
+--============================================= 
+-- Author:    Eric Born 
+-- Create date: 18 March 2019
+-- Last Modified:
+-- Description: Used to feed appointment data to power BI
+-- =============================================
+
+SELECT DISTINCT lm.location_name, 
+CASE 
+	WHEN r.Region IS NULL THEN 'Other' 
+	ELSE r.Region
+END AS 'Region'
+,CASE 
+	WHEN r.county IS NULL THEN 'Other' 
+	ELSE r.county
+END AS 'county'
+,COUNT(*) AS 'count'
+FROM ngprod.dbo.appointments a
+JOIN ngprod.dbo.location_mstr lm ON lm.location_id = a.location_id
+LEFT JOIN region r ON a.zip = r.Zip
+WHERE a.appt_date >= '20180101' AND a.appt_date <= '20181231'
+AND appt_kept_ind = 'y' 
+AND a.location_id NOT IN (
+ '096B6FF0-ED48-4A6C-95F6-8D37E1474394'
+,'7E8F1E17-1FC5-4019-B510-B7D3EC453D82'
+,'3A067539-F112-4304-931D-613F7C4F26FD'
+,'518024FD-A407-4409-9986-E6B3993F9D37')
+GROUP BY lm.location_name, r.Region, r.county
+HAVING COUNT (*) > 100
+ORDER BY location_name, region
